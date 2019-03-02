4bcec60b1e2fc2f0fa29e4fe1e21b0eea551113d
bySabi Files <bysabi.files@gmail.com>
2019-03-02 08:49:45 +0100

    update example config

---
+++ example/component/Link.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import NextJsLink from 'next/link';
+
+const assetPrefix = process.env.ASSET_PREFIX;
+
+const Link = ({ href, ...rest }) => (
+  <NextJsLink href={href} as={`${assetPrefix}${href}`} {...rest} />
+);
+
+export default Link;
--- example/pages/index.js
+++ example/pages/index.js
@@ -1,4 +1,4 @@
-import Link from 'next/link';
+import Link from '../component/Link';
 
 export default () => (
   <div style={{ textAlign: 'center' }}>
